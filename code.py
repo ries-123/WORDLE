e97467ff0449bb3f18a25031a6484f7e4740dd46
ries.123 <ries.123@osu.edu>
2026-02-13 05:58:01 -0500

    i think i finally did it *cries* - it being the full wordle experience as far as i can tell

--- code.py
+++ code.py
@@ -22,6 +22,7 @@ word = random.choice(astro_words).upper()
 word_letters = list(word)
 guesses_left = 6
 appended_guess = []
+n = 0 #this is the thing that will count up to test each letter (see below)
 
 ##print(word, word_letters) ##remove this l8r(test)
 
@@ -37,10 +38,30 @@ while guesses_left > 0: #later add a nested thing where api gives hint if guesse
             print("Please enter only letters.")
             continue
         break
-    guesses_left -= 1 #subtracts 1 from # of guesses left
+    if guess == word:
+        print(f"congratulations you did it!! the word was {word}")
+        break
+   
     appended_guess.append(guess) #makes list of guesses so far
     print(f"so far you have guessed: {appended_guess}, you have {guesses_left} guesses left") 
-    guess_letters = list(guess)
-    if guesses_left == 0:
+    guess_letters = list(guess) #turns guess into letters, separates them into diff strings to test
+    
+    for n in range(5): #tests each letter in the guess, n is the thing that counts up to test each letter
+        
+        if guess_letters[n] == word_letters[n]: #if the two first letters are the same
+            print(f"in {guess}, {guess_letters[n]} is correct and in the right position")
+        elif guess_letters[n] in word_letters:
+            print(f"in {guess}, {guess_letters[n]} is correct but in the wrong position")
+        else:
+            print(f"in {guess}, {guess_letters[n]} is not in the word")
+        n += 1 #adds 1 to n so it can test the next letter in the next loop
+            
+    n = 0 #resets n to 1 so it can test the next guess
+    guesses_left -= 1 #subtracts 1 from # of guesses left
+    if guesses_left <= 0:
         print(f"*extremely loud incorrect buzzer* nah bru it was {word}")
         break
+    
+
+
+
