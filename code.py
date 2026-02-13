95c80634c4bb36a39950e9a4c9824d933021b334
ries.123 <ries.123@osu.edu>
2026-02-13 02:47:56 -0500

    added words list

---
+++ code.py
@@ -0,0 +1,16 @@
+##importing ts
+import numpy as np
+import random
+
+## List of words
+astro_words = [
+    "Space", "Earth", "Lunar", "Solar",
+    "Orbit", "Light", "Night", "Comet", 
+    "Radio", "Pluto", "Alpha", "Venus",
+    "Phase", "Flare",
+    "Mount", "Aries", "Dwarf", "Orion",
+    "Titan", "Bulge", "Libra", "Virgo",
+    "Epoch", "Thebe", "Ceres", "Umbra", 
+    "Helix", "Deneb", "Nadir", "Apsis",
+
+    ]
