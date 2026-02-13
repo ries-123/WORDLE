7b0a6b607c0e343573cc70b8f31446c5b730704c
ries.123 <ries.123@osu.edu>
2026-02-13 03:57:54 -0500

    added thing to prompt user and split up user imput

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+##saving progress: git add ____, git commit -m "message", git push origin main
+
 ## importing ts
 import numpy as np
 import random
@@ -19,4 +21,22 @@ astro_words = [
 word = random.choice(astro_words).upper()
 word_letters = list(word)
 guesses_left = 6
-print(word, word_letters) ##remove this l8r
+
+
+##print(word, word_letters) ##remove this l8r(test)
+
+##loops until user imputs the right type of stuff
+print("AstWordle: Astronomy meets Wordle. you have 6 tries to guess a 5 letter word")
+while guesses_left > 0:
+    while True:
+        guess = input("Enter a 5-letter word: ").upper()
+        if len(guess) != 5:
+            print("Please enter exactly 5 letters.")
+            continue
+        if not guess.isalpha():
+            print("Please enter only letters.")
+            continue
+        break
+print(f"you guessed: {guess}")
+user_letters = list(guess)
+print(user_letters)
