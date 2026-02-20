c0e72ec830cf23a05a4a1b4ca21a743c224d5c2d
ries.123 <ries.123@osu.edu>
2026-02-19 19:23:15 -0500

    made colored letters white for readability

--- code.py
+++ code.py
@@ -103,27 +103,27 @@ while play=="yes":
 #win conditions, helps keep track of wins per guess stats
         if guess == word and guesses_so_far == 0:
             guess_on_one += 1
-            print(f"congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}")
+            print(f"\x1b[46m congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}\x1b[0m")
             break
         elif guess == word and guesses_so_far == 1:
             guess_on_two += 1
-            print(f"congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}")
+            print(f"\x1b[46m congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}\x1b[0m")
             break
         elif guess == word and guesses_so_far == 2:
             guess_on_three += 1
-            print(f"congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}")
+            print(f"\x1b[46m congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}\x1b[0m")
             break
         elif guess == word and guesses_so_far == 3:
             guess_on_four += 1
-            print(f"congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}")
+            print(f"\x1b[46m congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}\x1b[0m")
             break
         elif guess == word and guesses_so_far == 4:
             guess_on_five += 1
-            print(f"congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}")
+            print(f"\x1b[46m congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}\x1b[0m")
             break
         elif guess == word and guesses_so_far == 5:
             guess_on_six += 1
-            print(f"congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}")
+            print(f"\x1b[46m congratulations you did it!! the word was {word}. you guessed it on guess #{guesses_so_far + 1}\x1b[0m")
             break
 
         
@@ -138,16 +138,16 @@ while play=="yes":
             # Determine color code for this letter in this guess
             if guess_letters[n] == word_letters[n]:
                 color = '\x1b[102m'  # green
-                colored_letters.append(f"\x1b[102m{letter}\x1b[0m")
-                print(f"in {guess}, \x1b[102m{letter}\x1b[0m is correct and in the right position")
+                colored_letters.append(f"\x1b[97m\x1b[102m{letter}\x1b[0m")
+                print(f"in {guess}, \x1b[97m\x1b[102m{letter}\x1b[0m is correct and in the right position")
             elif letter in word_letters:
                 color = '\x1b[103m'  # yellow
-                colored_letters.append(f"\x1b[103m{letter}\x1b[0m")
-                print(f"in {guess}, \x1b[103m{letter}\x1b[0m is correct but in the wrong position")
+                colored_letters.append(f"\x1b[97m\x1b[103m{letter}\x1b[0m")
+                print(f"in {guess}, \x1b[97m\x1b[103m{letter}\x1b[0m is correct but in the wrong position")
             elif letter not in word_letters:
                 color = '\x1b[100m'  # gray
-                colored_letters.append(f"\x1b[100m{letter}\x1b[0m")
-                print(f"in {guess}, \x1b[100m{letter}\x1b[0m is not in the word")
+                colored_letters.append(f"\x1b[97m\x1b[100m{letter}\x1b[0m")
+                print(f"in {guess}, \x1b[97m\x1b[100m{letter}\x1b[0m is not in the word")
             else:
                 color = ''
             # Always update the color for the letter to the most recent
@@ -180,7 +180,7 @@ while play=="yes":
     print({guess_on_one}, {guess_on_two}, {guess_on_three}, {guess_on_four}, {guess_on_five}, {guess_on_six})
     guess_graph_x = ['1', '2', '3', '4', '5', '6'] #x axis for graph, will be used to show how many guesses it took to win
     guess_graph_y = [guess_on_one, guess_on_two, guess_on_three, guess_on_four, guess_on_five, guess_on_six] 
-    plt.bar(guess_graph_x, guess_graph_y, color='light_green') #makes the graph
+    plt.bar(guess_graph_x, guess_graph_y, color='green') #makes the graph
     plt.xlabel('Guess Number') #x axis label    
     plt.ylabel('Number of Wins') #y axis label
     plt.title("AstWordle Guess Distribution") #title of graph
